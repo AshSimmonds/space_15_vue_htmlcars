5bc15970bada9e32d9c7394c6e8e79df601295cf
AshSimmonds <ashsimmonds@gmail.com>
2022-02-13 03:34:10 +1030

    add dist

---
+++ dist/precache-manifest.9fb4acca4815c948674e515f1f4eb392.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b07ca191905b9412b3a",
+    "url": "/space_15_vue_htmlcars/js/about.c1d70782.js"
+  },
+  {
+    "revision": "0f6a1649850bb3448f41",
+    "url": "/space_15_vue_htmlcars/css/app.6c697eed.css"
+  },
+  {
+    "revision": "0f6a1649850bb3448f41",
+    "url": "/space_15_vue_htmlcars/js/app.d0e2b5a1.js"
+  },
+  {
+    "revision": "91137bf1d04c41682440",
+    "url": "/space_15_vue_htmlcars/css/chunk-vendors.a0cf6741.css"
+  },
+  {
+    "revision": "91137bf1d04c41682440",
+    "url": "/space_15_vue_htmlcars/js/chunk-vendors.ecb3bae2.js"
+  },
+  {
+    "revision": "0cd5f2c671dcc993bd214c49fdc8176e",
+    "url": "/space_15_vue_htmlcars/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/space_15_vue_htmlcars/robots.txt"
+  }
+];
